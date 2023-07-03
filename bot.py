9b0352046884853a943a97fa16fae623f4894546
Serey Ivanov <sergeyprok2@gmail.com>
2023-07-03 19:01:12 +0300

    26

--- bot.py
+++ bot.py
@@ -317,8 +317,8 @@ async def novosti_selenium(message: Message):
         # await message.answer(text='закончился time.sleep')
         # response = await page.goto("https://dzen.ru/?clid=1946579&win=90&yredirect=true&utm_referer=sso.dzen.ru")
         # response = await page.goto("https://dzen.ru/?yredirect=true")
-        response = await page.goto("https://stepik.org/lesson/716118/step/4?unit=716910")
-        # response = await page.goto("https://google.com")
+        # response = await page.goto("https://stepik.org/lesson/716118/step/4?unit=716910")
+        response = await page.goto("https://google.com")
         if response.status == 200:
             await message.answer(text='200')
         else:
