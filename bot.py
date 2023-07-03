d436bd5e60c05c5cb151ed4632259d5d04297ebb
Serey Ivanov <sergeyprok2@gmail.com>
2023-07-03 19:20:55 +0300

    28

--- bot.py
+++ bot.py
@@ -302,13 +302,13 @@ async def novosti_selenium(message: Message):
     # print(message.from_user.id)
     # try:
     async with async_playwright() as pw:
-        # proxy_server = {'server': 'http://195.216.135.182:8000', 'username': 'XvQx6z', 'password': '8k8KKM'}
+        proxy_server = {'server': 'http://195.216.135.182:8000', 'username': 'XvQx6z', 'password': '8k8KKM'}
         # proxy_server = {'server': 'http://195.216.135.182:8000', 'https': 'https://195.216.135.182:8000',}
         await message.answer(text='выполняет строку browser = await pw.chromium.launch(headless=True)')
         # browser = await pw.chromium.launch(headless=False,proxy=proxy_server)
         browser = await pw.chromium.launch(headless=True)
         await message.answer(text='выполняет строку context = await browser.new_context()')
-        context = await browser.new_context()
+        context = await browser.new_context(proxy=proxy_server)
         await message.answer(text='выполняет строку page = await context.new_page()')
         page = await context.new_page()
         await message.answer(text='заходит на страницу сайта')
